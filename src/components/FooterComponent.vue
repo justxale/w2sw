b03564ab9b6ab3e63d944d1f109b83fedc0f7826
Xale <alexev2401@gmail.com>
2024-08-11 00:39:53 +0300

    feat: add proper page footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,23 +1,102 @@
 <script setup lang="ts">
+type FooterLink = {
+    title: string,
+    link: string,
+}
+type FooterColumn = {
+    title: string,
+    links: FooterLink[]
+}
+
+const links: FooterColumn[] = [
+    {
+        title: 'Resources',
+        links: [
+            {title: "GitHub", link: "https://github.com/justxale/w2sw"},
+            {title: "Руководство", link: "https://github.com/justxale/w2sw/docs"},
+        ]
+    }
+]
 </script>
 
 <template>
     <footer>
         <div class="footer-container">
             <div class="footer-content">
-                Boo!
+                <div class="footer-logo-container">
+                    <img src="/w2sw-logo-full.png" class="footer-logo" alt=""/>
+                    <div class="footer-note">
+                        W2SW является проектом с <a href="https://github.com/justxale/w2sw" target="_blank">открытым исходым кодом</a>.<br/>
+                        &copy; justxale
+                    </div>
+                </div>
+                <div v-for="linkColumn in links" class="footer-column">
+                    <h4>{{ linkColumn.title }}</h4>
+                    <a v-for="link in linkColumn.links" :href="link.link">{{ link.title }}</a>
+                </div>
             </div>
+        </div>
+        <div class="footer-warning">
+            W2SW НЕ ЯВЛЯЕТСЯ ОФИЦИАЛЬНЫМ ПРОЕКТОМ УМ И НИКАК НЕ СВЯЗАН С АДМИНИСТРАЦИЕЙ.
         </div>
     </footer>
 </template>
 
 <style scoped>
 footer {
-    border-top: 1px var(--color-white-200) solid;
-    min-height: 700px;
-    background: radial-gradient(circle at -10% 130%, rgba(54, 228, 112, 1) 0%, transparent 40%),
-        radial-gradient(circle at 30% 140%, #A24BFF 0%, transparent 50%);
-    padding: 5vh 5vw;
+    max-height: 40vh;
+    background: radial-gradient(circle at 100% 150%, var(--color-brand-green) 0%, transparent 500px), radial-gradient(circle at 80% 170%, var(--color-brand-purple) 0%, transparent 500px), var(--color-gray-200);
+    display: flex;
+    flex-direction: column;
+
     margin-top: 128px;
+    font-size: 16px;
+}
+
+.footer-container {
+    padding: 2vh 5vw;
+}
+
+.footer-logo-container {
+    width: min-content;
+}
+
+.footer-logo {
+    height: 128px;
+    width: auto;
+}
+
+.footer-note {
+    font-size: 16px;
+    margin-left: 32px;
+    margin-top: 16px;
+}
+
+.footer-column {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+}
+
+.footer-column h4 {
+    margin: 0;
+}
+
+.footer-column a {
+    color: var(--color-white-100)
+}
+
+.footer-content {
+    display: flex;
+    margin-bottom: 128px;
+    gap: 256px;
+}
+
+
+
+.footer-warning {
+    color: var(--color-white-200);
+    align-self: center;
+    margin-bottom: 16px;
 }
 </style>
