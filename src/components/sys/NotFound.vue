dde524751b2e0d6fc286ff6be70227ccc6e32d1b
Xale <alexev2401@gmail.com>
2024-08-11 14:29:33 +0300

    feat: update 404 page with button

--- src/components/sys/NotFound.vue
+++ src/components/sys/NotFound.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
-
+document.title = 'Страница не найдена :< | W2SW'
 </script>
 
 <template>
 <div class="error-container">
-    Куда стучишься, нет страницы!
+    Страница не найдена :[
+    <RouterLink to="/">На главную</RouterLink>
 </div>
 </template>
 
 <style scoped>
 .error-container {
     display: flex;
+    flex-direction: column;
+    gap: 16px;
     justify-content: center;
     align-items: center;
 }
