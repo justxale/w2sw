fc6223a70cf6fe410ff6cfc42c898a1fe772afb0
Xale <alexev2401@gmail.com>
2024-08-11 13:51:47 +0300

    feat: add placeholder AllStructs page

--- src/components/wiki/AllStructs.vue
+++ src/components/wiki/AllStructs.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
+
+import {basicStructureData} from "../../structures/data.ts";
 </script>
 
 <template>
+    <div>
+        <h2>Структуры</h2>
+        <div class="struct-links">
+            <RouterLink :to="`/struct/${link.id}`" v-for="link in basicStructureData">{{ link.title }}</RouterLink>
+        </div>
+    </div>
 
 </template>
 
 <style scoped>
-
+.struct-links {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+}
 </style>
